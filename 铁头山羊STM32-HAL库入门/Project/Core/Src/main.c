575d66579b4adc918da01f70961d214cf65ba77f
Grid_Corp <you@example.com>
2025-12-23 10:47:49 +0800

    1.闪灯实验

--- 铁头山羊STM32-HAL库入门/Project/Core/Src/main.c
+++ 铁头山羊STM32-HAL库入门/Project/Core/Src/main.c
@@ -94,6 +94,10 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+		HAL_Delay(300);
+		HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+		HAL_Delay(300);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
