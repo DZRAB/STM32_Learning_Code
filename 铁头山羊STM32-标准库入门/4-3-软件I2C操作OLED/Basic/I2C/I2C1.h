c83fd8dbd935a048389d2aa15817b47d2a297303
Grid_Home <616444538@qq.com>
2025-12-30 21:38:21 +0800

    2025年12月30日21:38:20

---
+++ 铁头山羊STM32-标准库入门/4-3-软件I2C操作OLED/Basic/I2C/I2C1.h
@@ -0,0 +1,9 @@
+#ifndef __I2C1_H
+#define __I2C1_H
+#include "stm32f10x.h"     
+
+void I2C1_DefaultInit(void);
+void I2C1_RemapInit(void);
+int8_t I2C1_SendByte(uint8_t Addr, uint8_t *pData, uint16_t Size);
+int8_t I2C1_ReceiveByte(uint8_t Addr, uint8_t *pData, uint16_t Size);
+#endif
