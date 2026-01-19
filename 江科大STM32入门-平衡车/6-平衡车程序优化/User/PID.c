6d8cc2e06e8674ed9efee7a5c6dcd662ceab1f6f
Grid_Home <616444538@qq.com>
2026-01-19 20:31:09 +0800

    2026年1月19日20:31:07

--- 江科大STM32入门-平衡车/6-平衡车程序优化/User/PID.c
+++ 江科大STM32入门-平衡车/6-平衡车程序优化/User/PID.c
@@ -8,20 +8,30 @@ void PID_Update(PID_t *p)
 	p->Error0 = p->Target - p->Actual;
 	
 	if(fabs(p->Ki) > 0.0001)
+	{
 		p->ErrorInt += p->Error0;
+		if(p->ErrorInt > p->ErrorIntMAX){p->ErrorInt = p->ErrorIntMAX;}
+		if(p->ErrorInt < p->ErrorIntMIN){p->ErrorInt = p->ErrorIntMIN;}
+	}
 	else
 		p->ErrorInt = 0;
 	
 	p->Out = p->Kp * p->Error0
 					+ p->Ki * p->ErrorInt
-					+ p->Kd * (p->Error0 - p->Error1);
+//				+ p->Kd * (p->Error0 - p->Error1);
+					- p->Kd * (p->Actual - p->Actual1);		
+	
+	if(p->Out > 0){p->Out += p->OutOffset;}
+	if(p->Out < 0){p->Out -= p->OutOffset;}
 	
 	if(p->Out > p->OutMAX){p->Out = p->OutMAX;}
 	if(p->Out < p->OutMIN){p->Out = p->OutMIN;}
+	
+	p->Actual1 = p->Actual;
 }
 
 void PID_Init(PID_t *p)
 {
 	p->Actual = p->Target = p->Error0 = p->Error1 = 
-	p->ErrorInt = p->Out = 0;
+	p->ErrorInt = p->Out = p->Actual1 = 0;
 }
